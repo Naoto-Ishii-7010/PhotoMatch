9911e04e279f95cf0c7c5095100c7e51baaaa86e
Naoto Ishii <leave.it.to.7010@gmail.com>
2026-05-06 14:56:43 +0900

    refactor: 新しいディレクトリ構成に基づき既存のディレクトリ構成を修正＋新規ディレクトリの作成

--- app/(marketing)/lp/layout.tsx
+++ app/(marketing)/lp/layout.tsx
@@ -1,6 +1,6 @@
 import type { Metadata } from "next";
-import LpHeader from "../../components/features/lp/LpHeader";
-import LpFooter from "../../components/features/lp/LpFooter";
+import LpHeader from "../../../features/lp/components/LpHeader";
+import LpFooter from "../../../features/lp/components/LpFooter";
 
 /**
  * LP専用レイアウト
