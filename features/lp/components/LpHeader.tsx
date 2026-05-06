e4ba40745a20b4de762bdaad3d77606da450d2da
Naoto Ishii <leave.it.to.7010@gmail.com>
2026-05-06 17:30:20 +0900

    feat: Google認証とDBを接続

--- features/lp/components/LpHeader.tsx
+++ features/lp/components/LpHeader.tsx
@@ -50,7 +50,7 @@ export default function LpHeader() {
         {/* CTA */}
         <div className="flex items-center gap-2.5">
           <div className="hidden sm:block">
-            <GoogleSignInButton variant="lpHeader" callbackUrl="/">
+            <GoogleSignInButton variant="lpHeader">
               Googleでログイン
             </GoogleSignInButton>
           </div>
